63462266b4ed9e18fe22e2e1d6434a2ce791bd43
theo-baccam <theo.baccam@laplateforme.io>
2024-09-06 16:39:13 +0200

    feat: moveTiles method, will refactor and fix further

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,8 +1,16 @@
+#define MIN_INDEX 0
+#define MAX_INDEX 3
+#define GRID_SIZE 4
+
+
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <functional>
 
 #include "Grid.hpp"
+
+#include <iostream>
 
 
 Grid::Grid() {
@@ -16,8 +24,8 @@ Grid::Grid() {
 Grid::~Grid() {}
 
 void Grid::initializeGrid() {
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
+    for (int y = MIN_INDEX; y < GRID_SIZE; y++) {
+        for (int x = MIN_INDEX; x < GRID_SIZE; x++) {
             if (this->grid[y][x] == 0) {
                 continue;
             }
@@ -37,8 +45,8 @@ void Grid::spawnNewTile() {
     int randomY;
 
     do {
-        randomY = rand() % 4;
-        randomX = rand() % 4;
+        randomY = rand() % GRID_SIZE;
+        randomX = rand() % GRID_SIZE;
     } while (this->grid[randomY][randomX] != 0);
 
     int randomValue = (rand() % 2 + 1) * 2;
@@ -46,4 +54,109 @@ void Grid::spawnNewTile() {
     this->grid[randomY][randomX] = randomValue;
 }
 
-void Grid::moveTiles(enum direction moveDirection) {}
+void Grid::moveTiles(enum direction moveDirection) {
+    int y;
+    int x;
+    int loopStart;
+    int* loop1;
+    int* loop2;
+
+    std::function<bool()> loop1Condition;
+    std::function<bool()> loop2Condition;
+    std::function<void()> loop1Increment;
+    std::function<void()> loop2Increment;
+    int yAhead;
+    int xAhead;
+
+    switch (moveDirection) {
+        case LEFT:
+            loop1Condition = [&]() {return y < GRID_SIZE;};
+            loop2Condition = [&]() {return x < GRID_SIZE;};
+            loop1Increment = [&]() {y++;};
+            loop2Increment = [&]() {x++;};
+            yAhead = 0;
+            xAhead = -1;
+            loop1 = &y;
+            loop2 = &x;
+            loopStart = MIN_INDEX;
+            break;
+        case RIGHT:
+            loop1Condition = [&]() {return y >= MIN_INDEX;};
+            loop2Condition = [&]() {return x >= MIN_INDEX;};
+            loop1Increment = [&]() {y--;};
+            loop2Increment = [&]() {x--;};
+            yAhead = 0;
+            xAhead = 1;
+            loop1 = &y;
+            loop2 = &x;
+            loopStart = MAX_INDEX;
+            break;
+        case DOWN:
+            loop1Condition = [&]() {return x < GRID_SIZE;};
+            loop2Condition = [&]() {return y < GRID_SIZE;};
+            loop1Increment = [&]() {x++;};
+            loop2Increment = [&]() {y++;};
+            yAhead = 1;
+            xAhead = 0;
+            loop1 = &x;
+            loop2 = &y;
+            loopStart = MIN_INDEX;
+            break;
+        case UP:
+            loop1Condition = [&]() {return x >= MIN_INDEX;};
+            loop2Condition = [&]() {return y >= MIN_INDEX;};
+            loop1Increment = [&]() {x--;};
+            loop2Increment = [&]() {y--;};
+            yAhead = -1;
+            xAhead = 0;
+            loop1 = &x;
+            loop2 = &y;
+            loopStart = MAX_INDEX;
+            break;
+    }
+
+    bool change;
+    do {
+        change = false;
+
+        *(loop1) = loopStart;
+        for (;loop1Condition(); loop1Increment()) {
+            *(loop2) = loopStart;
+            for (;loop2Condition(); loop2Increment()) {
+                // blank square
+                if (grid[y][x] == 0) {
+                    continue;
+                }
+
+                // wall collision
+                if (y + yAhead < MIN_INDEX   
+                    || y + yAhead > MAX_INDEX
+                    || x + xAhead < MIN_INDEX
+                    || x + xAhead > MAX_INDEX
+                ) {
+                    continue;
+                }
+
+                // No fusion collision
+                if (
+                    grid[y + yAhead][x + xAhead] != 0
+                    && grid[y + yAhead][x + xAhead] != grid[y][x]
+                ) {
+                    continue;
+                }
+
+                change = true;
+
+                // fusion
+                if (grid[y + yAhead][x + xAhead] == grid[y][x]) {
+                    grid[y + yAhead][x + xAhead] *= 2;
+                    grid[y][x] = 0;
+                    continue;
+                }
+
+                grid[y + yAhead][x + xAhead] = grid[y][x];
+                grid[y][x] = 0;
+            }
+        }
+    } while (change);
+}
--- tests/test_Grid.cpp
+++ tests/test_Grid.cpp
@@ -3,6 +3,8 @@
 #include <doctest.h>
 #include "Grid.hpp"
 
+#include <iostream>
+
 
 TEST_CASE("Checking if the grid is initialized correctly") {
     Grid g;
@@ -26,4 +28,54 @@ TEST_CASE("Checking if the grid is initialized correctly") {
     bool gridSumCorrect = (gridSum == 4 || gridSum == 6 || gridSum == 8);
     INFO("The sum of squares on the grid is ", gridSum);
     CHECK(gridSumCorrect);
+}
+
+TEST_CASE("Checking if tile movement works") {
+    Grid g;
+
+    SUBCASE("Moving one square") {
+        g.grid[0][3] = 2;
+
+        REQUIRE(g.grid[0][3] == 2);
+
+        g.moveTiles(LEFT);
+        CHECK(g.grid[0][0] == 2);
+
+        g.moveTiles(DOWN);
+        CHECK(g.grid[3][0] == 2);
+
+        g.moveTiles(RIGHT);
+        CHECK(g.grid[3][3] == 2);
+
+        g.moveTiles(UP);
+        CHECK(g.grid[0][3] == 2);
+    }
+
+    SUBCASE("Uncompatible square collision") {
+        g.grid[0][3] = 2;
+        g.grid[0][0] = 4;
+
+        REQUIRE(g.grid[0][0] == 4);
+        REQUIRE(g.grid[0][3] == 2);
+
+        g.moveTiles(LEFT);
+        CHECK(g.grid[0][0] == 4);
+        CHECK(g.grid[0][1] == 2);
+        CHECK(g.grid[0][3] == 0);
+    }
+
+    SUBCASE("Square fusion") {
+        g.grid[0][0] = 4;
+        g.grid[0][1] = 2;
+        g.grid[0][3] = 2;
+
+        REQUIRE(g.grid[0][0] == 4);
+        REQUIRE(g.grid[0][1] == 2);
+        REQUIRE(g.grid[0][3] == 2);
+
+        g.moveTiles(LEFT);
+        CHECK(g.grid[0][0] == 4);
+        CHECK(g.grid[0][1] == 4);
+        CHECK(g.grid[0][3] == 0);
+    }
 }
