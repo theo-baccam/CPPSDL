d5f94ed87d1339781e1ec331f079608f084359f4
Chaima BEKHAOUDA <chaima.bekhaouda@laplateforme.io>
2024-09-09 12:40:17 +0200

    feat(grid): add new_grid function to initialize grid

--- include/grid.hpp
+++ include/grid.hpp
@@ -6,3 +6,12 @@
 using Grid = std::array<std::array<int, 4>, 4>;
 
 #endif
+
+Grid new_grid()
+{
+    Grid grid =
+       for {int x = 0; x < 4; x++}
+           for (int y = 0; y < 4; y++)
+               grid[x][y] = 0;
+    return grid;
+}
